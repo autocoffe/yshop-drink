df368963ca7c19d64b2881f759baa31348154f8c
zhuohaofan <zhuohaofan@aliyun.com>
2025-08-30 23:39:01 +0800

    [uniapp]: fix compile error, replace flyio with uni.request

--- yshop-drink-uniapp-vue3/api/api.js
+++ yshop-drink-uniapp-vue3/api/api.js
@@ -1,165 +1,165 @@
-// #ifdef H5
-// h5端
-import Fly from 'flyio/dist/npm/fly'
-// #endif
-
-// #ifdef APP-PLUS
-// app端
-import Fly from 'flyio/dist/npm/wx'
-// #endif
-
-// #ifdef MP-WEIXIN
-import Fly from 'flyio/dist/npm/wx'
-// #endif
-
+// api.js - 基于 uni.request 的统一请求封装
 import { handleLoginFailure } from '@/utils'
 import { isWeixin } from '@/utils/util'
 import { VUE_APP_API_URL } from '@/config'
 import cookie from '@/utils/cookie'
 import { replace } from '@/utils/router'
 
-const fly = new Fly()
-fly.config.baseURL = VUE_APP_API_URL
+// 创建 request 函数容器
+const request = {}
 
-fly.interceptors.response.use(
-  response => {
-    // console.log(response)
-    // 定时刷新access-token
-    return response
-  },
-  error => {
-    if (error.toString() == 'Error: Network Error') {
-      handleLoginFailure()
-      return Promise.reject({ msg: '未登录', toLogin: true })
-    }
-    if (error.status == 401) {
-      handleLoginFailure()
-      return Promise.reject({ msg: '未登录', toLogin: true })
-    }
-    if (error.response.data.status == 5109) {
-      uni.showToast({
-        title: error.response.data.msg,
-        icon: 'none',
-        duration: 2000,
-      })
-    }
-    return Promise.reject(error)
-  }
-)
-
+// 基础配置
 const defaultOpt = { login: true }
 
-function baseRequest(options) {
+// HTTP 方法集合
+const METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD']
+
+// 请求拦截器（可扩展）
+function requestInterceptor(options) {
+  // 添加 baseURL
+  if (!options.url.startsWith('http')) {
+    options.url = VUE_APP_API_URL + options.url
+  }
+
+  // 设置默认 header
+  options.header = {
+    'Content-Type': 'application/json',
+    ...options.header
+  }
+
+  // 登录态处理
   const token = cookie.get('accessToken')
-   console.log('--> % token % token:\n', token)
-
-  options.headers = {
-    ...options.headers,
+  if (options.login !== false && token) {
+    options.header.Authorization = 'Bearer ' + token
   }
 
-  // if (options.login === true) {
-  options.headers = {
-    ...options.headers,
-    Authorization: 'Bearer ' + token,
-  }
-  // }
-
-  // 结构请求需要的参数
-  const { url, params, data, login, ...option } = options
-
-  // 发起请求
-  return fly
-    .request(url, params || data, {
-      ...option,
-    })
-    .then(res => {
-     
-      const data = res.data || {}
-	   //console.log('res.status:',res)
-    // console.log('res.code:',res.code)
-	 // #ifdef H5
-	 if (res.data.code == 1004004002) {
-		 if(isWeixin()){
-			const url = cookie.get('index_url')
-			console.log('redirect_uri:',url)
-			//const url = `${location.origin}/h5/#/pages/index/index`
-			location.href = url
-			return
-		}
-      }
-	  // #endif
-	  
-      if (res.status !== 200) {
-        return Promise.reject({ msg: '请求失败', res, data })
-      }
-
-    
-      if (data.code == 401) {
-        uni.hideLoading()
-        handleLoginFailure()
-        uni.showToast({
-          title: data.msg,
-          icon: 'none',
-          duration: 2000,
-        })
-        return Promise.reject({ msg: data.msg, res, data })
-      }
-
-      if (data.code != 0) {
-        uni.showToast({
-          title: data.msg,
-          icon: 'none',
-          duration: 2000,
-        })
-        return Promise.reject({ data, res })
-      }
-
-
-      return Promise.resolve(data.data, res)
-
-      // if ([401, 403].indexOf(data.status) !== -1) {
-      //   handleLoginFailure()
-      //   return Promise.reject({ msg: res.data.msg, res, data, toLogin: true })
-      // } else if (data.status === 200) {
-      //   return Promise.resolve(data, res)
-      // } else if (data.status == 5101) {
-      //   return Promise.reject({ msg: res.data.msg, res, data })
-      // } else {
-      //   return Promise.reject({ msg: res.data.msg, res, data })
-      // }
-    })
+  console.log('--> request token:', token)
+  return options
 }
 
-/**
- * http 请求基础类
- * 参考文档 https://www.kancloud.cn/yunye/axios/234845
- *
- */
-const request = ['post', 'put', 'patch'].reduce((request, method) => {
-  /**
-   *
-   * @param url string 接口地址
-   * @param data object get参数
-   * @param options object axios 配置项
-   * @returns {AxiosPromise}
-   */
-  request[method] = (url, data = {}, options = {}) => {
-    console.log(url, data)
-    return baseRequest(Object.assign({ url, data, method }, defaultOpt, options))
-  }
-  return request
-}, {})
+// 响应拦截器
+function responseInterceptor(res, resolve, reject) {
+  const data = res.data || {}
 
-;['get', 'delete', 'head'].forEach(method => {
-  /**
-   *
-   * @param url string 接口地址
-   * @param params object get参数
-   * @param options object axios 配置项
-   * @returns {AxiosPromise}
-   */
+  // #ifdef H5
+  if (data.code === 1004004002) {
+    if (isWeixin()) {
+      const url = cookie.get('index_url') || `${location.origin}/h5/#/pages/index/index`
+      console.log('redirect_uri:', url)
+      location.href = url
+      return
+    }
+  }
+  // #endif
+
+  // 状态码非 200
+  if (res.statusCode !== 200) {
+    uni.showToast({
+      title: '请求失败',
+      icon: 'none',
+      duration: 2000,
+    })
+    return reject({ msg: '请求失败', res, data })
+  }
+
+  // 业务逻辑错误处理
+  if (data.code === 401) {
+    uni.hideLoading()
+    handleLoginFailure()
+    uni.showToast({
+      title: data.msg || '未登录',
+      icon: 'none',
+      duration: 2000,
+    })
+    return reject({ msg: data.msg, res, data, toLogin: true })
+  }
+
+  if (data.code !== 0) {
+    uni.showToast({
+      title: data.msg || '请求异常',
+      icon: 'none',
+      duration: 2000,
+    })
+    return reject({ msg: data.msg, res, data })
+  }
+
+  // 成功
+  return resolve(data.data, res)
+}
+
+// 通用请求方法
+function baseRequest(options) {
+  return new Promise((resolve, reject) => {
+    // 构造请求参数
+    const {
+      url,
+      method = 'GET',
+      data = {},
+      params = {},
+      login = true,
+      ...customOptions
+    } = options
+
+    let requestUrl = url
+    let requestData = method.toUpperCase() === 'GET' ? params : data
+
+    // 合并配置
+    const reqConfig = requestInterceptor({
+      url: requestUrl,
+      method,
+      data: requestData,
+      ...customOptions,
+      login // 控制是否携带 token
+    })
+
+    // 发起请求
+    uni.request({
+      url: reqConfig.url,
+      method: reqConfig.method,
+      data: reqConfig.data,
+      header: reqConfig.header,
+      success: (res) => {
+        responseInterceptor(res, resolve, reject)
+      },
+      fail: (err) => {
+        console.error('Network Error:', err)
+
+        // 网络错误或超时
+        if (err.errMsg.includes('network') || err.errMsg.includes('timeout')) {
+          handleLoginFailure()
+          return reject({ msg: '网络错误', toLogin: true })
+        }
+
+        reject({ msg: '请求失败', err })
+      },
+    })
+  })
+}
+
+// 动态挂载 POST、PUT、PATCH
+;['post', 'put', 'patch'].forEach((method) => {
+  request[method] = (url, data = {}, options = {}) => {
+    console.log('[Request]', url, data)
+    return baseRequest({
+      url,
+      method: method.toUpperCase(),
+      data,
+      ...defaultOpt,
+      ...options,
+    })
+  }
+})
+
+// 动态挂载 GET、DELETE、HEAD
+;['get', 'delete', 'head'].forEach((method) => {
   request[method] = (url, params = {}, options = {}) => {
-    return baseRequest(Object.assign({ url, params, method }, defaultOpt, options))
+    return baseRequest({
+      url,
+      method: method.toUpperCase(),
+      params,
+      ...defaultOpt,
+      ...options,
+    })
   }
 })
 
